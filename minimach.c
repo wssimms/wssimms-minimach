4956dcb337b79ad80b2fe8e06acb9dfd520e0357
William Simms <wssimms@gmail.com>
2025-02-02 11:17:19 +0900

    interim commit - add absolute operands to arith/logical instructions

--- minimach.c
+++ minimach.c
@@ -28,6 +28,10 @@
  * 12 TEST  X,Y,Z        PC = PC + 1 + (A<0 ? sext(X)
  *                                          : (A=0 ? sext(Y)
  *                                                 : sext(Z)))
+ * 14 AND   address
+ * 15 OR    address
+ * 19 ADD   address
+ * 20 SUB   address
  *
  * Memory Map
  *   0x0000 - 0xEFFF : read/write memory
@@ -70,7 +74,134 @@ uint8_t program[] = {
     39, 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 46, 10, 0
 };
 
+FILE *diskf = 0;
+
 uint64_t cycles = 0;
+
+uint8_t  track = 0;
+uint8_t  sector = 0;
+uint16_t address = 0;
+
+enum disk_state { DISK_IDLE=0, DISK_SUCCESS=1, DISK_ERROR=2, DISK_BUSY=4 } state;
+
+#define DISK_ID_BYTE '2';
+
+uint8_t rd (uint16_t a);
+void wr (uint16_t a, uint8_t b);
+
+FILE * disk_file (void)
+{
+    static char *default_name = "minimach.dsk";
+
+    if (diskf == 0) {
+	diskf = fopen(default_name, "r+b");
+	if (diskf == 0) {
+	    diskf = fopen(default_name, "w+b");
+	    if (diskf) {
+		if (fseek(diskf, 2*1024*1024 - 1, SEEK_SET)) {
+		    fclose(diskf);
+		    diskf = 0;
+		}
+		else if (fwrite("\0", 1, 1, diskf) != 1) {
+		    fclose(diskf);
+		    diskf = 0;
+		}
+	    }
+	}
+    }
+
+    if (diskf) {
+	long offs = track * 8192 + sector * 512;
+	if (fseek(diskf, offs, SEEK_SET))
+	    state = DISK_ERROR;
+    }
+    
+    return diskf;
+}
+
+void get_buf (uint16_t a, uint8_t *buf)
+{
+    for (int i = 0; i < 512; ++i)
+	buf[i] = rd((uint16_t)(a+i));
+}
+
+void put_buf (uint16_t a, uint8_t *buf)
+{
+    for (int i = 0; i < 512; ++i)
+	wr((uint16_t)(a+i), buf[i]);
+}
+
+uint8_t disk_rd (uint16_t a)
+{
+    if (a == 0) {
+	return DISK_ID_BYTE;
+    }
+    else if (a == 1) {
+	return 0;        // lo byte of # of tracks (256)
+    }
+    else if (a == 2) {
+	return 1;        // hi byte of # of tracks (256)
+    }
+    else if (a == 3) {
+	return 16;       // # of 512 byte sectors per track
+    }
+    else if (a == 4)	{
+	uint8_t b = (uint8_t)state;
+	state = DISK_IDLE;
+	//printf("status: %d\n", b);
+	return b;
+    }
+    return 0;
+}
+
+void disk_wr (uint16_t a, uint8_t b)
+{
+    if (a == 0) {
+	track = b;
+	//printf("track: %d\n", track);
+    }
+    else if (a == 1) {
+	sector = b & 15;
+	//printf("sector: %d\n", sector);
+    }
+    else if (a == 2) {
+	address = (address & 0xFF00) | b;
+	//printf("(1) address: %d\n", address);
+    }
+    else if (a == 3) {
+	address = (address & 0x00FF) | (b << 8);
+	//printf("(2) address: %d\n", address);
+    }
+    else if (a == 4) {
+	static uint8_t buf[512];
+
+	if (state != DISK_IDLE) return;
+	state = DISK_BUSY;
+	
+	FILE *f = disk_file();
+	if (f == 0 || state == DISK_ERROR) return;
+	
+	    
+	if (b & 1) {
+	    //printf("write\n");
+	    get_buf(address, buf);
+	    if (fwrite(buf, 1, 512, f) != 512) {
+		state = DISK_ERROR;
+		return;
+	    }
+	}
+	else {
+	    //printf("read\n");
+	    if (fread(buf, 1, 512, f) != 512) {
+		state = DISK_ERROR;
+		return;
+	    }
+	    put_buf(address, buf);
+	}
+	
+	state = DISK_SUCCESS;
+    }
+}
 
 uint8_t rd (uint16_t a)
 {
@@ -82,8 +213,13 @@ uint8_t rd (uint16_t a)
     if (a < 0xFF00) return rom[a - 0xFF00];
     
     switch (a & 255) {
-    case 0: return getchar();
-    default: return 0;
+    case 0:
+	return getchar();
+    default:
+	if ((a & 0xFFF8) == 0xFF10)
+	    return disk_rd(a & 15);
+	else
+	    return 0;
     }
 }
 
@@ -99,8 +235,12 @@ void wr (uint16_t a, uint8_t b)
     if (a < 0xFF00) return;
 
     switch (a & 255) {
-    case 0: putchar(b); return;
-    default: return;
+    case 0:
+	putchar(b); return;
+    default:
+	if ((a & 0xFFF8) == 0xFF10)
+	    return disk_wr(a & 15, b);
+
     }
 }
 
@@ -111,7 +251,7 @@ void reset (void)
     pc.b[1] = rd(0xFEFF);
 }
 
-//#define DBG 500
+//#define DBG 100000
 
 void execute (void)
 {
@@ -129,6 +269,7 @@ void execute (void)
         case 0:                                      /* E */
 	    printf("End at pc = %d   Total # cycles = %llu\n", pc.w-1, cycles);
             return;
+	    
         case 1:                                      /* L */
             tm.b[0] = rd(pc.w++);
             tm.b[1] = rd(pc.w++);
@@ -137,6 +278,7 @@ void execute (void)
 	    printf("L    %5d (%2.2x/%3d) ", tm.w, ac.b[0], ac.b[0]);
 #endif
 	    break;
+	    
         case 2:                                      /* S */
             tm.b[0] = rd(pc.w++);
             tm.b[1] = rd(pc.w++);
@@ -145,6 +287,7 @@ void execute (void)
 	    printf("S    %5d (%2.2x/%3d) ", tm.w, ac.b[0], ac.b[0]);
 #endif
             break;
+	    
         case 3:                                      /* SW */
             tm.b[0] = ac.b[0];
             ac.b[0] = ac.b[1];
@@ -153,51 +296,61 @@ void execute (void)
 	    printf("SWAP                ");
 #endif
             break;
-        case 4:                                      /* AND */
+
+	    /*
+        case 4:                                      // AND
             ac.b[0] = ac.b[0] & ac.b[1];
             ac.b[1] = ~(ac.b[0]);
 #ifdef DBG
 	    printf("AND                 ");
 #endif
             break;
-        case 5:                                      /* OR */
+	    
+        case 5:                                      // OR
             ac.b[0] = ac.b[0] | ac.b[1];
             ac.b[1] = ~(ac.b[0]);
 #ifdef DBG
 	    printf("OR                  ");
 #endif
             break;
-        case 6:                                      /* EOR */
+	    
+        case 6:                                      // EOR
             ac.b[0] = ac.b[0] ^ ac.b[1];
             ac.b[1] = ~(ac.b[0]);
 #ifdef DBG
 	    printf("EOR                 ");
 #endif
             break;
+	    */
+	    
         case 7:                                      /* SHL */
             ac.w <<= 1;
 #ifdef DBG
 	    printf("SHL                 ");
 #endif
             break;
+	    
         case 8:                                      /* SHR */
 	    ac.w >>= 1;
 #ifdef DBG
 	    printf("SHR                 ");
 #endif
             break;
-        case 9:                                      /* ADD */
+	    /*
+        case 9:                                      // ADD
 	    ac.w = EXT(ac.b[0]) + EXT(ac.b[1]);
 #ifdef DBG
 	    printf("ADD                 ");
 #endif
             break;
-        case 10:                                     /* SUB */
+	    
+        case 10:                                     // SUB
             ac.w = EXT(ac.b[0]) - EXT(ac.b[1]);
 #ifdef DBG
 	    printf("SUB                 ");
 #endif
             break;
+	    */
         case 11:                                     /* J */
             tm.b[0] = rd(pc.w++);
             tm.b[1] = rd(pc.w++);
@@ -207,6 +360,7 @@ void execute (void)
 	    printf("JUMP %5d          ", tm.w);
 #endif
             break;
+	    
         case 12:                                     /* TST */
 	    if (((int16_t)SEXT(ac.b[0])) < 0)
                 tm.w = SEXT(rd(pc.w+0));
@@ -219,7 +373,49 @@ void execute (void)
 	    printf("TEST  %4.4x          ", SEXT(ac.b[0]));
 #endif
             break;
+	    
+        case 14:                                     /* AND abs */
+            tm.b[0] = rd(pc.w++);
+            tm.b[1] = rd(pc.w++);
+            ac.b[0] = ac.b[0] & rd(tm.w);
+            ac.b[1] = ~(ac.b[0]);
+#ifdef DBG
+	    printf("AND  %5d          ", tm.w);
+#endif
+	    break;
+	    
+        case 15:                                     /* OR abs  */
+            tm.b[0] = rd(pc.w++);
+            tm.b[1] = rd(pc.w++);
+            ac.b[0] = ac.b[0] | rd(tm.w);
+            ac.b[1] = ~(ac.b[0]);
+#ifdef DBG
+	    printf("OR   %5d          ", tm.w);
+#endif
+	    break;
+	    
+        case 19:                                     /* ADD abs */
+            tm.b[0] = rd(pc.w++);
+            tm.b[1] = rd(pc.w++);
+	    ac.w = EXT(ac.b[0]) + EXT(rd(tm.w));
+#ifdef DBG
+	    printf("ADD  %5d          ", tm.w);
+#endif
+	    break;
+	    
+        case 20:                                     /* SUB abs */
+            tm.b[0] = rd(pc.w++);
+            tm.b[1] = rd(pc.w++);
+	    ac.w = EXT(ac.b[0]) - EXT(rd(tm.w));
+#ifdef DBG
+	    printf("SUB  %5d          ", tm.w);
+#endif
+	    break;
+	    
         default:
+#ifdef DBG
+	    printf("??? (%2d)              ", opcode);
+#endif
 	    reset();
             break;
         }
@@ -296,6 +492,27 @@ int options (int argc, char **argv)
 	    }
 	    else
 		fprintf(stderr, "No filename specified for '-r' option.\n");
+	}
+	else if (0 == strcmp(argv[i], "-d")) {
+	    if (++i < argc) {
+		diskf = fopen(argv[i], "r+b");
+		if (diskf == 0) {
+		    diskf = fopen(argv[i], "w+b");
+		    if (diskf) {
+			if (fseek(diskf, 2*1024*1024 - 1, SEEK_SET)) {
+			    fclose(diskf);
+			    diskf = 0;
+			}
+			else if (fwrite("\0", 1, 1, diskf) != 1) {
+			    fclose(diskf);
+			    diskf = 0;
+			}
+		    }
+		}
+		printf("1b) disk file: %p\n", diskf);
+	    }
+	    else
+		fprintf(stderr, "Can't open disk file '%s' for reading.\n", argv[i]);
 	}
 	else if (prog)
 	    fprintf(stderr, "Multiple program files specified. Using '%s'\n", argv[prog]);
